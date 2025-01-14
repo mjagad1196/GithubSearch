7b19d2b4297cd2411f2cf28db68d6e8d299d9334
mjagad1196 <mihirjagad@gmail.com>
2025-01-14 22:47:47 +0530

    Lint Action

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "coms.speer.githubsearch"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "coms.speer.githubsearch"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
